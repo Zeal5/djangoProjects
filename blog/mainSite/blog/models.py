89149f36e70281b34ed2bf9a98922577e73a5dcc
Zeal5 <tassadiqshakeel5@gmail.com>
2025-03-05 11:45:41 +0500

    Start Comments Section

--- blog/mainSite/blog/models.py
+++ blog/mainSite/blog/models.py
@@ -13,7 +13,7 @@ class PublishedManager(models.Manager):
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
-    slug = models.TextField(max_length=250,unique_for_date= 'publish')
+    slug = models.TextField(max_length=250, unique_for_date="publish")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -43,7 +43,25 @@ class Post(models.Model):
         return str(self.title)
 
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.publish.year,
-                                                 self.publish.month,
-                                                 self.publish.day,
-                                                 self.slug])
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
+
+
+class Comment(models.Model):
+    # post name email updated commented created active
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment")
+    body = models.TextField()
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["created"]
+        indexes = [models.Index(fields=["created"])]
+
+    def __str__(self) -> str:
+        return str(f"Commented By {self.name} on {self.post.title}")
