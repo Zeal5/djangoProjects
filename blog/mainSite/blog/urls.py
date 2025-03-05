ea595dac60b3e2b12b4631d8af9d0d0c3fdad3ba
Zeal5 <tassadiqshakeel5@gmail.com>
2025-03-05 10:49:41 +0500

    Add Send Email Functionality

--- blog/mainSite/blog/urls.py
+++ blog/mainSite/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import PostListView, post_detail
+from blog.views import PostListView, post_detail, post_share
 
 
 app_name = "blog"
@@ -9,4 +9,5 @@ urlpatterns = [
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>", post_detail, name="post_detail"
     ),
+    path('<int:post_id>/share/',post_share, name='post_share'),
 ]
--- blog/mainSite/blog/views.py
+++ blog/mainSite/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from blog.forms import EmailPostForm
+from django.core.mail import send_mail
 from blog.models import Post
+from mainSite import settings
+import json
 
 """
 def post_list(request):
@@ -17,6 +21,8 @@ def post_list(request):
 
     return render(request, "blog/post/list.xhtml", {"posts": posts})
 """
+
+
 class PostListView(ListView):
     template_name = "blog/post/list.xhtml"
     queryset = Post.published.all()
@@ -24,7 +30,32 @@ class PostListView(ListView):
     paginate_by = 3
 
 
+def post_share(request, post_id):
+    sent = False
+    post = get_object_or_404(Post, id=post_id)
+    
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
 
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " f"{post.title}"
+
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+
+            # send_mail(subject, message, settings.EMAIL_HOST_USER, [cd["to"]])
+            print(subject, message, settings.EMAIL_HOST_USER, [cd["to"]])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(
+        request, "blog/post/share.xhtml", {"post": post, "form": form, "sent": sent}
+    )
 
 
 def post_detail(request, year, month, day, post):
