ee11549b0c130d7306115b1458d1d820e9a50e41
Zeal5 <tassadiqshakeel5@gmail.com>
2025-03-31 16:21:51 +0500

    Change Admin panel for User

--- blog/mainSite/user/admin.py
+++ blog/mainSite/user/admin.py
@@ -3,4 +3,10 @@ from user.models import User
 # Register your models here.
 
 
-admin.site.register(User)
+# admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username','email', 'is_staff', 'role']
+    list_filter = ['is_staff', 'role']
+    search_fields = ['username', 'first_name', 'last_name']
+    ordering = ['date_joined']
